444dc64154f63df1cd0bc1897936495cb477f424
Luca Marzano <lmarzano@student.42roma.it>
2021-10-18 21:48:18 +0200

    megaphone is up and running. starting ex01

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/18 19:13:05 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/10/18 19:28:42 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/10/18 21:31:45 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,26 @@
 
 int	main(int argc, char **argv)
 {
-	//check args (1 or 2)
-	//parse argv[1]
-	//convert argv[1]'s lowercase to uppercase
+	int		i;
+	int		j;
+	char	c;
 
-	//* if argc == 1,  megaphone will return "* LOUD AND UNBEARABLE FEEDBACK NOISE *" in a new line
+	j = 1;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	(void)argv;
+	while (argv[j])
+	{
+		i = 0;
+		while (argv[j][i])
+		{
+			c = toupper(argv[j][i]);
+			std::cout << c;
+			i++;
+		}
+		j++;
+		if (j)
+			std::cout << " ";
+	}
+	std::cout << std::endl;
 	return (0);
 }
