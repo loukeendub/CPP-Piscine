f3d70720b88ebefd213275954e5101a4d53089ab
Lou Keen <74676663+loukeendub@users.noreply.github.com>
2021-10-24 23:51:23 +0200

    Update Account.cpp

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,70 +6,103 @@
 Account::Account( int initial_deposit )//	constructor
 {
 	//	init class values
-	//	print "[timestamp] index:(_accountIndex);amount:(_amount);created"
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";created" << std::endl;
 	return ;
 }
 
 Account::~Account( void )//	destructor
 {
-	//	print "[timestamp] index:(_accountIndex);amount:(_amount);closed"
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_amount << ";closed" << std::endl;
 	return ;
 }
 
-static int	Account::getNbAccounts( void )//	returns the value of private var _nbAccounts
+static int	Account::getNbAccounts( void )
 {
 	return (this->_nbAccounts);
 }
 
-static int	Account::getTotalAmount( void )//	returns the value of private var _totalAmounts
+static int	Account::getTotalAmount( void )
 {
 	return (this->_totalAmount);
 }
 
-static int	Account::getNbDeposits( void ))//	returns the value of private var _totalNbDeposits
+static int	Account::getNbDeposits( void ))
 {
 	return (this->_totalNbDeposits);
 }
 
-static int	getNbWithdrawals( void ))//	returns the value of private var _totalNbWithdrawals
+static int	Account::getNbWithdrawals( void ))
 {
 	return (this->_totalNbWithdrawals);
 }
 
-static void	displayAccountsInfos( void )
+static void	Account::displayAccountsInfos( void )
 {
-	//	print "[timestamp] accounts:(_nbAccounts);total:(_totalAmount);deposits:(_totalNbDeposits);withdrawals:(_totalNbWithdrawals)"
+	this->_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total:" << getTotalAmount() << ";";
+	std::cout << "deposits:" << getNbDeposits()) << ";";
+	std::cout << "withdrawals:" << getNbWithdrawals << std::endl;
 	return ;
 }
 
 void	Account::makeDeposit( int deposit )
 {
-	//	print "[timestamp] index:(_accountIndex);p_amount:(_amount);"
-	//	_amount + deposit 
-	//	print "deposit:(deposit);amount:(_amount);nb_deposits:(_nbDeposits)"
+	this->_displayTimestamp(); 
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "p_amount:" << this->_amount << ";";
+	this->_amount += deposit;
+	std::cout << "deposit:" << deposit << ";";
+	std::cout << "amount:" << this->_amount << ";";
+	std::cout << "nb_deposits:" << this->_nbDeposits << std::endl;
 	return ;
 }
 
 bool	Account::makeWithdrawal( int withdrawal )
 {
-	//	if _totalAmount < withdrawal
-	//		print "[timestamp] index:(_accountIndex);p_amount:(_amount);withdrawal:refused"
-	//		return (false);
-	//	else
-	//		print "[timestamp] index:(_accountIndex);p_amount:(_amount);"
-	//		_amount - withdrawal
-	//		print "withdrawal:(withdrawal);amount:(_amount);nb_withdrawals:(_nbWithdrawals)"
-	//		return (true);
+	int	tmp_amount = this->_amount;
+
+	this->_displayTimestamp(); 
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "p_amount:" << this->_amount << ";";
+	this->_amount -= withdrawal;
+	if (checkAmount() == 1)
+	{
+		this->_nbWithdrawals++;
+		std::cout << "withdrawal:" << withdrawal << ";";
+		std::cout << "amount:" << this->_amount << ";";
+		std::cout << "nb_withdrawals:" << this->_nbWithdrawals << std::endl;
+		return (true);
+	}
+	else
+	{
+		this->_amount = tmp_amount;
+		return (false);
+	}
 }
 
 int	Account::checkAmount( void ) const
 {
-
+	if (this->_amount < 0)
+	{
+		std::cout << "withdrawal:refused" << std::endl;
+		return (0)
+	}
+	else
+		return(1)
 }
 
 void	Account::displayStatus( void ) const
 {
-	//	print "[timestamp] index:(_accountIndex);amount:(_amount);deposits:(_nbDeposits);withdrawals:(_nbWithdrawals)"
+	this->_displayTimestamp();
+	std::cout << "index:" << this->_accountIndex << ";";
+	std::cout << "amount:" << this->_totalAmount << ";";
+	std::cout << "deposits:" << this->_nbDeposits << ";";
+	std::cout << "withdrawals:" << this->_nbWithdrawals << std::endl;
 }
 
 //	[PRIVATE :]
