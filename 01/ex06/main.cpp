bc62858baba04eb1ace023ca4918821ff86b745c
Lou Keen <74676663+loukeendub@users.noreply.github.com>
2021-11-04 09:16:37 +0100

    Update main.cpp

--- 01/ex06/main.cpp
+++ 01/ex06/main.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
 #include "Karen.hpp"
 
+int	checkComplaint(std::string complaint)
+{
+	if (complaint == "DEBUG")
+		return (1);
+	else if (complaint == "INFO")
+		return (2);
+	else if (complaint == "WARNING")
+		return (3);
+	else if (complaint == "ERROR")
+		return (4);
+	else
+		return (0);
+}
+
 int main(int argc, char** argv)
 {
 	Karen   karen;
 
 	if (argc != 2)
-		return(1);//	error: invalid number of arguments
-	switch (argv[1])
 	{
-	case "DEBUG":
+		std::cout << "Error : Invalid number of arguments" << std::endl;
+		return(1);
+	}
+	switch (checkComplaint(argv[1]))
+	{
+	case 1:
 		karen.complain("debug");
 		break;
-	case "INFO":
+	case 2:
 		karen.complain("info");
 		break;
-	case "WARNING":
+	case 3:
 		karen.complain("warning");
 		break;
-	case "ERROR":
+	case 4:
 		karen.complain("error");
 		break;
 	
