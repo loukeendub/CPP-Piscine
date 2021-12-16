b512826b56c2296edc6262698d28676957e065ba
Luca Marzano <lmarzano@student.42roma.it>
2021-12-16 22:49:56 +0100

    main update

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 22:38:56 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/12/16 22:48:07 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/12/16 22:49:47 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,21 @@ int main()
 main must output something like this :
 
 ./convert 0
+
 char: Non displayable
 int: 0
 float: 0.0f
 double: 0.0
+
 ./convert nan
+
 char: impossible
 int: impossible
 float: nanf
 double: nan
+
 ./convert 42.0f
+
 char: '*'
 int: 42
 float: 42.0f
