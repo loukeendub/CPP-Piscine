fc1a0eafffbc6044caa2eaac2f091ad97c024c64
Luca Marzano <lmarzano@student.42roma.it>
2021-12-16 22:48:38 +0100

    updated main with expected output in comments

--- 06/ex00/main.cpp
+++ 06/ex00/main.cpp
@@ -6,7 +6,35 @@
 /*   By: lmarzano <lmarzano@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 22:38:56 by lmarzano          #+#    #+#             */
-/*   Updated: 2021/12/16 22:45:42 by lmarzano         ###   ########.fr       */
+/*   Updated: 2021/12/16 22:48:07 by lmarzano         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+
+int main()
+{
+    return (0);
+}
+
+/*
+
+main must output something like this :
+
+./convert 0
+char: Non displayable
+int: 0
+float: 0.0f
+double: 0.0
+./convert nan
+char: impossible
+int: impossible
+float: nanf
+double: nan
+./convert 42.0f
+char: '*'
+int: 42
+float: 42.0f
+double: 42.0
+
+/*
