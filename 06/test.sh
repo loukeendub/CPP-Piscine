7d0609724355142f7e8f9cfcd8886f2ef98c25a9
Lou Keen <74676663+loukeendub@users.noreply.github.com>
2021-12-17 13:05:23 +0100

    added tester

---
+++ 06/test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -ex
+mkdir -p result
+./scalar_conversion '0' > 'result/0'
+./scalar_conversion 'nan' > 'result/nan'
+./scalar_conversion '42.0f' > 'result/42.0f'
+./scalar_conversion 'inf' > 'result/inf'
+./scalar_conversion 'n' > 'result/n'
+./scalar_conversion 'nanf' > 'result/nanf'
+./scalar_conversion '42' > 'result/42'
+./scalar_conversion '42.042' > 'result/42.042'
+./scalar_conversion '4.2f' > 'result/4.2f'
+diff answer result
+rm -r result
