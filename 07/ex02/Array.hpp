7abbf7beeeda87abb1c49e14a2bf5876c166fc3f
Lou Keen <74676663+loukeendub@users.noreply.github.com>
2021-12-29 19:25:19 +0100

    Update Array.hpp

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -9,84 +9,147 @@ class Array
 {
 	private:
 		T*				arr;
-		unsigned int	size;
+		unsigned int	_size;
 
 		class OutOfLimitsException : public std::exception
 		{
 			public:
-				const char* what() const throw();
+				const char* what() const throw()
+				{
+					return ("Error: Out of range limit.");
+				}
 		};
 	public:
-		Array();
-		Array(unsigned int n);
-		Array(const Array& copy);
-		~Array();
+		//Array();
+		//Array(unsigned int n);
+		//Array(const Array& copy);
+		//~Array();
 
-		Array&		operator = (const Array& op);
-		T&			operator [] (unsigned int n);
-		const T&	operator [] (unsigned int n)	const;
+		//Array&		operator = (const Array& op);
+		//T&			operator [] (unsigned int n);
+		//const T&	operator [] (unsigned int n)	const;
 
-		unsigned int	size()	const;
+		//unsigned int	size()	const;
+
+		Array() :
+		_size(0)
+		{
+			this->arr = new[0];
+		}
+
+		Array(unsigned int n) :
+		_size(n)
+		{
+			this->arr = new[n];
+			for (unsigned int i = 0; i < this->_size; i++)
+				this->arr[i] = T();
+		}
+
+		Array(const Array& copy)
+		{
+			*this = copy;
+		}
+
+		~Array()
+		{
+			delete[] this->arr;
+		}
+
+		Array&	operator = (const Array& op)
+		{
+			if (this == &op)
+				return (*this);
+			delete[] this->arr;
+			this->arr = new T[op._size];
+			this->_size = op._size;
+			for (unsigned int i = 0; i < this->_size; i++)
+				this->arr[i] = op.arr[i];
+			return (*this);
+		}
+
+		T&	operator [] (unsigned int n)
+		{
+			if (n >= this->_size)
+				throw OutOfLimitsException();
+			return (this->arr[n]);
+		}
+
+		const T&	operator [] (unsigned int n)	const
+		{
+			if (n >= this->_size)
+				throw OutOfLimitsException();
+			return (this->arr[n]);
+		}
+
+		unsigned int	size()	const
+		{
+			return (this->_size);
+		}
+
+		const char*	OutOfLimitsException::what() const throw()
+		{
+			return ("Error: Out of range limit.");
+		}
 };
 
-Array::Array() :
-size(0)
-{
-	this->arr = new[0];
-}
+//Array::Array() :
+//_size(0)
+//{
+//	this->arr = new[0];
+//}
+//
+//Array::Array(unsigned int n) :
+//_size(n)
+//{
+//	this->arr = new[n];
+//	for (unsigned int i = 0; i < this->_size; i++)
+//		this->arr[i] = T();
+//}
+//
+//Array::Array(const Array& copy)
+//{
+//	*this = copy;
+//}
+//
+//Array::~Array()
+//{
+//	delete[] this->arr;
+//}
+//
+//Array&	Array::operator = (const Array& op)
+//{
+//	if (this == &op)
+//		return (*this);
+//	delete[] this->arr;
+//	this->arr = new T[op._size];
+//	this->_size = op._size;
+//	for (unsigned int i = 0; i < this->_size; i++)
+//		this->arr[i] = op.arr[i];
+//	return (*this);
+//}
+//
+//T&	Array::operator [] (unsigned int n)
+//{
+//	if (n >= this->_size)
+//		throw OutOfLimitsException();
+//	return (this->arr[n]);
+//}
 
-Array::Array(unsigned int n) :
-size(n)
-{
-	this->arr = new[n];
-	for (unsigned int i = 0; i < this->size; i++)
-		this->arr[i] = T();
-}
+//const T&	Array::operator [] (unsigned int n)	const
+//{
+//	if (n >= this->_size)
+//		throw OutOfLimitsException();
+//	return (this->arr[n]);
+//}
 
-Array::Array(const Array& copy)
-{
-	*this = copy;
-}
-
-Array::~Array()
-{
-	delete[] this->arr;
-}
-
-Array&	Array::operator = (const Array& op)
-{
-	if (this == &op)
-		return (*this);
-	delete[] this->arr;
-	this->arr = new T[op.size];
-	this->size = op.size;
-	for (unsigned int i = 0; i < this->size; i++)
-		this->arr[i] = op.arr[i];
-	return (*this);
-}
-
-T&	Array::operator [] (unsigned int n)
-{
-	if (n >= this->size)
-		throw OutOfLimitsException();
-	return (this->arr[n]);
-}
-
-const T&	Array::operator [] (unsigned int n)	const
-{
-	if (n >= this->size)
-		throw OutOfLimitsException();
-	return (this->arr[n]);
-}
-
-unsigned int	Array::size()	const
-{
-	return (this->size);
-}
-
-const char*	Array::OutOfLimitsException::what() const throw()
-{
-	return ("Error: Out of range limit.");
-}
+//unsigned int	Array::size()	const
+//{
+//	return (this->_size);
+//}
+//
+//const char*	Array::OutOfLimitsException::what() const throw()
+//{
+//	return ("Error: Out of range limit.");
+//}
 
 #endif
