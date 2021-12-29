caf78adc104df3feaa2b88a77cf5b88235681d06
Lou Keen <74676663+loukeendub@users.noreply.github.com>
2021-12-29 19:06:05 +0100

    ex02 not tested

--- 07/ex02/Array.hpp
+++ 07/ex02/Array.hpp
@@ -1,20 +1,92 @@
 #ifndef ARRAY_HPP
 # define ARRAY_HPP
 
+# include <iostream>
+# include <exception>
+
 template <class T>
 class Array
 {
-private:
-    /* data */
-public:
-    Array(/* args */);
-    ~Array();
+	private:
+		T*				arr;
+		unsigned int	size;
+
+		class OutOfLimitsException : public std::exception
+		{
+			public:
+				const char* what() const throw();
+		};
+	public:
+		Array();
+		Array(unsigned int n);
+		Array(const Array& copy);
+		~Array();
+
+		Array&		operator = (const Array& op);
+		T&			operator [] (unsigned int n);
+		const T&	operator [] (unsigned int n)	const;
+
+		unsigned int	size()	const;
 };
 
-Array::Array(/* args */)
-{}
+Array::Array() :
+size(0)
+{
+	this->arr = new[0];
+}
+
+Array::Array(unsigned int n) :
+size(n)
+{
+	this->arr = new[n];
+	for (unsigned int i = 0; i < this->size; i++)
+		this->arr[i] = T();
+}
+
+Array::Array(const Array& copy)
+{
+	*this = copy;
+}
 
 Array::~Array()
-{}
+{
+	delete[] this->arr;
+}
+
+Array&	Array::operator = (const Array& op)
+{
+	if (this == &op)
+		return (*this);
+	delete[] this->arr;
+	this->arr = new T[op.size];
+	this->size = op.size;
+	for (unsigned int i = 0; i < this->size; i++)
+		this->arr[i] = op.arr[i];
+	return (*this);
+}
+
+T&	Array::operator [] (unsigned int n)
+{
+	if (n >= this->size)
+		throw OutOfLimitsException();
+	return (this->arr[n]);
+}
+
+const T&	Array::operator [] (unsigned int n)	const
+{
+	if (n >= this->size)
+		throw OutOfLimitsException();
+	return (this->arr[n]);
+}
+
+unsigned int	Array::size()	const
+{
+	return (this->size);
+}
+
+const char*	Array::OutOfLimitsException::what() const throw()
+{
+	return ("Error: Out of range limit.");
+}
 
 #endif
--- 07/ex02/main.cpp
+++ 07/ex02/main.cpp
@@ -1,6 +1,117 @@
 #include "Array.hpp"
 
-int	main()
+void	testEmpty()
 {
-	return (0);
+	Array<int>	emptyArr;
+
+	std::cout << "Empty Array address: " << &emptyArr << std::endl;
+	std::cout << "Empty Array [0]: " << emptyArr[0] << std::endl;
+}
+
+void	testInt()
+{
+	Array<int>	intArr(5);
+	
+	for (int i = 0; intArr[i]; i++)
+		std::cout << intArr[i] << " ";
+	std::cout << std::endl;
+
+	for (int i = 0; intArr[i]; i++)
+		intArr[i] = i + 1;
+	std::cout << std::endl;
+
+	for (int i = 0; intArr[i]; i++)
+		std::cout << intArr[i] << " ";
+	std::cout << std::endl;
+}
+
+void	testString()
+{
+	Array<std::string>	strArr(5);
+	
+	for (int i = 0; strArr[i]; i++)
+		std::cout << strArr[i] << "," << std::endl;
+
+	for (int i = 0; strArr[i]; i++)
+		strArr[i] = "Array position ";
+
+	for (int i = 0; strArr[i]; i++)
+		std::cout << strArr[i] << i << std::endl;
+}
+
+void	testAssign()
+{
+	Array<int>	first(5);
+
+	for (int i = 0; first[i]; i++)
+		first[i] = i + 1;
+	
+	Array<int>	second;
+
+	second = first;
+	for (int i = 0; second[i]; i++)
+		std::cout << second[i] << " ";
+	std::cout << std::endl;
+}
+
+void	testCopy()
+{
+	Array<int>	first(5);
+
+	for (int i = 0; first[i]; i++)
+		first[i] = i + 1;
+	
+	Array<int>	second(first);
+
+	for (int i = 0; second[i]; i++)
+		std::cout << second[i] << " ";
+	std::cout << std::endl;
+}
+
+
+int	checkInput(std::string input)
+{
+	if (input == "empty")
+		return (1);
+	else if (input == "int")
+		return (2);
+	else if (input == "string")
+		return (3);
+	else if (input == "assign")
+		return (4);
+	else if (input == "copy")
+		return (5);
+	else
+		return (0);
+}
+
+int main()
+{
+	std::string	input;
+
+	std::cout << "Please select an option:" << std::endl;
+	std::cout << "<main> <swap> <min> <max>" << std::endl;
+	std::getline(std::cin, input, '\n');// gets input value
+
+	switch (checkInput(input))
+	{
+	case 1:
+		testEmpty();
+		return (0);
+	case 2:
+		testInt();
+		return (0);
+	case 3:
+		testString();
+		return (0);
+	case 4:
+		testAssign();
+		return (0);
+	case 5:
+		testCopy();
+		return (0);
+	default:
+		std::cout << "Error: Invalid input!" << std::endl;
+		return (1);
+	}
 }
